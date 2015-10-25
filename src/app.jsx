c5a4c164d2a9fdccddbbde80589cd420cb05d400
Rob Uridge <ruridge@gmail.com>
2015-10-25 17:16:40 +0000

    3: Introduction to Properties

--- src/app.jsx
+++ src/app.jsx
@@ -2,14 +2,28 @@ var React = require('react');
 var ReactDOM = require('react-dom');
 
 var App = React.createClass({
+  getDefaultProps: function() {
+    return {
+      text: 'this is a default prop',
+      cat: 0
+    };
+  },
+  propTypes: {
+    text: React.PropTypes.string,
+    cat: React.PropTypes.number.isRequired
+  },
   render: function(){
+    var text = this.props.text;
     return (
       <div>
         <b>BOLD</b>
-        <h1>Hello World</h1>
+        <h1>{text}</h1>
       </div>
     );
   }
 });
 
-ReactDOM.render(<App />, document.body);
+ReactDOM.render(
+  <App cat={5} />,
+  document.getElementById('content')
+);
